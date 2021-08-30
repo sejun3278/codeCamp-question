3750c5798c05f30bfe4c101cf6e814a48772f056
Kim Sejun <sejun3278@gmail.com>
2021-08-30 23:05:16 +0900

    2021-08-30-condition-complate

--- 3일차 - 조건문 설명/script.js
+++ 3일차 - 조건문 설명/script.js
@@ -31,10 +31,25 @@ const pageMove = ( page ) => {
 }
 
 const selectFunction = ( number ) => {
-    if(a !== number) {
+    // if(a !== number) {
         document.getElementsByClassName('select')[0].classList.remove('select');
         document.getElementById('select_' + number).classList.add('select');
         
         a = number;
-    }
+
+        getObjResult();
+    // }
+}
+
+const getObjResult = () => {
+    console.log(a)
+    let div = `<div> <b class=${a > 1 ? 'yellow' : 'red'}> if( a > 1 ) { `;
+        div += `<div style="padding-left : 40px; padding-top : 10px" > a 가 1 보다 크다. </div>`;
+        div += `<div style="padding-top : 30px"> } </b> <b class=${a === 1 ? 'yellow' : a === 0 && 'red' }> else if( a === 1 ) { </div>`;
+        div += `<div style="padding-left : 40px; padding-top : 10px" > a 와 1 이 같다. </div>`;
+        div += `<div style="padding-top : 30px"> } </b> <b class=${a < 1 && 'yellow'}> else { </div>`;
+        div += `<div style="padding-left : 40px; padding-top : 10px" > a 가 1 보다 작다. </div>`;
+    div += `} </b> </div>`;
+
+    document.getElementById('condition_result_showing_wrapper').innerHTML = div;
 }
